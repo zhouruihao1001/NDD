8049ea7d88a0dbf19609dccd80469e9d94400d10
zhouruihao1001 <103633232+zhouruihao1001@users.noreply.github.com>
2022-07-29 16:42:27 +0800

    Add files via upload

---
+++ cpp/include/NDD.h
@@ -0,0 +1,106 @@
+#pragma once
+
+#include <ctime>
+#include <cassert>
+#include <cmath>
+#include <utility>
+#include <vector>
+#include <algorithm> 
+#include <cstdlib>
+#include <memory>
+#include <iostream>
+
+#include <Eigen/Dense>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/eigen.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+#include "nanoflann.hpp"
+#include "KDTreeVectorOfVectorsAdaptor.h"
+
+#include "tictoc.h"
+
+using namespace Eigen;
+using namespace nanoflann;
+
+using std::cout;
+using std::endl;
+using std::make_pair;
+
+using std::atan2;
+using std::cos;
+using std::sin;
+
+using NDDPointType = pcl::PointXYZI; // using xyz only. but a user can exchange the original bin encoding function.
+using KeyMat = std::vector<std::vector<float> >;
+using InvKeyTree = KDTreeVectorOfVectorsAdaptor< KeyMat, float >;
+
+
+void coreImportTest ( void );
+
+
+// sc param-independent helper functions 
+float xy2theta( const float & _x, const float & _y );
+MatrixXd circshift( MatrixXd &_mat, int _num_shift );
+std::vector<float> eig2stdvec( MatrixXd _eigmat );
+
+
+class NDDManager
+{
+public: 
+    NDDManager( ) = default; // reserving data space (of std::vector) could be considered. but the descriptor is lightweight so don't care.
+
+    Eigen::MatrixXd makeNDD( pcl::PointCloud<NDDPointType> & _scan_down );
+    Eigen::MatrixXd AlignmentKeyFromNDD( Eigen::MatrixXd &_desc );
+    Eigen::MatrixXd SearchingKeyFromNDD( Eigen::MatrixXd &_desc );
+
+    int fastAlignUsingVkey ( MatrixXd & _vkey1, MatrixXd & _vkey2 ); 
+    double CorrBtnNDD ( MatrixXd &_ndd1, MatrixXd &_ndd2 ); 
+    std::pair<double, int> distanceBtnNDD ( MatrixXd &_ndd1, MatrixXd &_ndd2 ); 
+
+    // User-side API
+    void makeAndSaveNDDAndKeys( pcl::PointCloud<NDDPointType> & _scan_down );
+    std::pair<int, float> detectLoopClosureID( void ); // int: nearest node index, float: relative yaw  
+
+public:
+    
+    const int    PC_NUM_RING = 20; // 20 rings
+    const int    PC_NUM_SECTOR = 60; // 60 sectors
+    const double PC_MAX_RADIUS = 80.0; // 80 meter max  range
+    const double PC_UNIT_SECTORANGLE = 360.0 / double(PC_NUM_SECTOR);
+    const double PC_UNIT_RINGGAP = PC_MAX_RADIUS / double(PC_NUM_RING);
+    const int max_num_in_cell = 30;//normally 5-50 points in one cell after down-sampling.
+
+    // tree
+    const int    NUM_EXCLUDE_RECENT = 100; // simply just keyframe gap, but node position distance-based exclusion is ok. 
+    const int    NUM_CANDIDATES_FROM_TREE = 20; // 10-25 is enough. 
+
+    // loop thres
+    const double SEARCH_RATIO = 0.1; // for fast comparison, no Brute-force, but search 10 % is okay. 
+
+    const double DIST_THRES = 0.3; // 0.3-0.4 is good choice for hard threshold
+
+    // config 
+    const int    TREE_MAKING_PERIOD_ = 10; // i.e., remaking tree frequency, to avoid non-mandatory every remaking, to save time cost / in the LeGO-LOAM integration, it is synchronized with the loop detection callback (which is 1Hz) so it means the tree is updated evrey 10 sec. But you can use the smaller value because it is enough fast ~ 5-50ms wrt N.
+    int          tree_making_period_conter = 0;
+
+    // data 
+    std::vector<double> polarcontexts_timestamp_; // optional.
+    std::vector<Eigen::MatrixXd> polarcontexts_;
+    std::vector<Eigen::MatrixXd> polarcontext_invkeys_;
+    std::vector<Eigen::MatrixXd> polarcontext_vkeys_;
+
+    KeyMat polarcontext_invkeys_mat_;
+    KeyMat polarcontext_invkeys_to_search_;
+    std::unique_ptr<InvKeyTree> polarcontext_tree_;
+
+}; // NDDManagers
+
+

d29e23147c390e33b0357fd6a9bb1ae979be0636
Hao <103633232+zhouruihao1001@users.noreply.github.com>
2023-01-31 18:08:29 +0800

    Update NDD_P.m

--- src/NDD_P.m
+++ src/NDD_P.m
@@ -1,10 +1,10 @@
-function[ D ] = NDD_P( ptcloud,N_sector,N_ring,range )
+function[ img ] = NDD_P( ptcloud,num_sector,num_ring,max_range )
 %% Downsampling
-gridStep = 0.75; % uniform downsampling(m). 
-ptcloud = pcdownsample(ptcloud, 'gridAverage', gridStep);
 % pcshow(ptcloud);
+gridStep = 0.75; % cubic grid downsampling(m). 
+ptcloud = pcdownsample(ptcloud, 'gridAverage', gridStep);
 
-%% PCA
+%% pca
  [V,~,eig] = pca(ptcloud.Location);
 if eig(2,:)/3 > eig(3,:)
     fd= V(:,3);
@@ -14,126 +14,60 @@ else
     fd = [1,0,0];
 end
 R = [V(:,1),V(:,2),cross(V(:,1),V(:,2))];
-
+% R = [cross(V(:,3),V(:,2)),V(:,2),V(:,3)];
 fd = R*fd;%feature direction
 yaw = atan( fd(2,:)/fd(1,:))+pi/2;
 A = [cos(yaw) sin(yaw) 0 ; ...
     -sin(yaw) cos(yaw) 0 ; ...
     0 0 1 ];
-ptcloud_rot =ptcloud.Location*A;
+ptcloud_tran =ptcloud.Location*A;
 
+%% Save a point to the corresponding bin 
+angle_one_sector = 360/num_sector;
+[theta,r] = cart2pol(ptcloud_tran(:,1),ptcloud_tran(:,2));
+theta = theta + pi;
 
-%% cell bin
-num_points = ptcloud.Count;
-% num_points = length(ptcloud);
-gap = range / N_ring; 
-angle_one_sector = 360/N_sector;
+r_step = max_range / num_ring;
+r_index = ceil(r/r_step);
+r_index(r_index>20) = 20;
 
-cell_bins = cell(N_ring, N_sector);
-cell_bin_counter = ones(N_ring, N_sector);
+s_step = angle_one_sector*pi/180;
+theta(theta>2*pi-eps) = 0;
+s_index = ceil(theta/s_step);
+s_index(s_index<1) = 1;
 
-enough_large = 500; % max points in cell
-enough_small = -1000;
+img = zeros(num_ring, num_sector);
 
-for ith_ring = 1:N_ring
-   for ith_sector = 1:N_sector
-        bin_ij = enough_small * ones(enough_large, 3);
-        cell_bins{ith_ring, ith_sector} = bin_ij;
-   end
-end
-%% Cell Dividing
-for ith_point =1:num_points
-
-    % Point information
-    ith_point_xyz = ptcloud_rot(ith_point,:);
-    ith_point_r = sqrt(ith_point_xyz(1)^2 + ith_point_xyz(2)^2);
-    ith_point_theta = XY2Theta(ith_point_xyz(1), ith_point_xyz(2)); % degree
-    
-    % Find the corresponding ring index 
-    tmp_ring_index = floor(ith_point_r/gap);
-    if(tmp_ring_index >= N_ring)
-        ring_index = N_ring;
-    else
-        ring_index = tmp_ring_index + 1;
-    end
-    
-    % Find the corresponding sector index 
-    tmp_sector_index = ceil(ith_point_theta/angle_one_sector);
-    if(tmp_sector_index == 0)
-        sector_index = 1;
-    elseif(tmp_sector_index > N_sector || tmp_sector_index < 1)
-        sector_index = N_sector;
-    else
-        sector_index = tmp_sector_index;
-    end
-    
-    % Assign point to the corresponding bin cell 
-    try
-        corresponding_counter = cell_bin_counter(ring_index, sector_index); % 1D real value.
-    catch
-        continue;
-    end
-    cell_bins{ring_index, sector_index}(corresponding_counter, :) = ith_point_xyz;
-    cell_bin_counter(ring_index, sector_index) = cell_bin_counter(ring_index, sector_index) + 1; % increase count 1
-    
-end
-%% Descriptor
-D = zeros(N_ring, N_sector);
-N = 3;
-min_num_thres = 5; %  noise-filter para.
-
-% Description
-for ith_ring = 1:N_ring
-    for ith_sector = 1:N_sector
-        points_in_cell = cell_bins{ith_ring, ith_sector};
-        if( num_minpoints(points_in_cell, min_num_thres, enough_small) )
-            for i = 1:enough_large
-                if(points_in_cell(i,1) == enough_small) 
-                    num_incell = i-1;
-                    break;
-                end                
-            end
-                points_in_cell = points_in_cell(1:num_incell,:);
-                %Normal Distribution
-                points_in_cell_mean = mean(points_in_cell);
-                points_in_cell_cov = cov(points_in_cell);
-                %Prevent covariance matrix from going singular (and not be invertible)
-                [U,S,V] = svd(points_in_cell_cov);
+for i=1:num_ring
+    for j=1:num_sector
+        idx = r_index== i & s_index==j;
+        point = ptcloud_tran(idx,:);
+        if size(point,1) > 4
+             points_in_bin_mean = mean(point);
+             points_in_bin_cov = cov(point);
+                [U,S,V] = svd(points_in_bin_cov);
                 if S(3,3) < 0.001 * S(1,1)
                     S(3,3) = 0.001 * S(1,1);
-                    points_in_cell_cov = U*S*V';
+                    points_in_bin_cov = U*S*V';
                 end
-                  [~, posDef] = chol(points_in_cell_cov);                
+                  [~, posDef] = chol(points_in_bin_cov);                
                 if posDef ~= 0
                     % If the covariance matrix is not positive definite,
                     % disregard the contributions of this cell.
                     continue;
                 end
-
-                % Probability Density
-                q = points_in_cell - points_in_cell_mean; 
-                gaussianValue =exp( -q / points_in_cell_cov * q'/2);
-                score = trace(gaussianValue);     
+                %entropy
+                %N = 4;
+                %entropy = (N/2)*(1+log(2*pi)) + 0.5*log(det(points_in_bin_cov));% log
+%                 points_in_bin_covinv = inv(points_in_bin_cov);
+                q = point - points_in_bin_mean; 
+                gaussianValue = -q / points_in_bin_cov * q'/2;
+                score = sum(exp(diag(gaussianValue)));
         else
+            %entropy = 0;
             score = 0;
         end
-        D(ith_ring, ith_sector)= score;
+        img(i, j)= score;
     end
 end
-
-
-end
-
-
-%%
-function bool = num_minpoints(mat, minimum_thres, enough_small)
-
-min_thres_point = mat(minimum_thres, :);
-
-if( isequal(min_thres_point, [enough_small, enough_small, enough_small]) )
-    bool = 0;
-else
-    bool = 1;
-end
-
 end
